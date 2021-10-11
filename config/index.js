78769ae7a0860e3a375a7f4616fc9a043b8ab9d8
hajidalakhtar <hajidalakhtar@gmail.com>
2021-10-11 14:43:01 +0000

    first commit

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   //-- SITE SETTINGS -----
-  author: "@konstantinmuenster",
+  author: "Muhammad Hajid Al Akhtar",
   siteTitle: "Gatsby Starter Portfolio Minimal",
-  siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
+  siteShortTitle: "HAJID", // Used as logo text in header, footer, and splash screen
   siteDescription:
     "A modern one-page portfolio with a clean yet expressive design.",
   siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
@@ -58,21 +58,17 @@ module.exports = {
   socialMedia: [
     {
       name: "Linkedin",
-      url: "https://www.linkedin.com/in/konstantin-muenster/",
-    },
-    {
-      name: "Medium",
-      url: "https://konstantinmuenster.medium.com/",
+      url: "https://www.linkedin.com/in/hajidalakhtar/",
     },
     {
       name: "Github",
-      url:
-        "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal",
+      url: "https://github.com/hajidalakhtar",
     },
     {
-      name: "Behance",
-      url: "https://www.behance.net/konstanmnster",
+      name: "Instagram",
+      url: "https://www.instagram.com/hajid_alakhtar/",
     },
+
     // {
     //   name: "Mail",
     //   url: "mailto:mail@konstantin.digital",
