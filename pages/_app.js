d81fcc65938a9b4de59a97ca1755045510fb2175
Hajidalakhtar <hajidalakhtar@gmail.com>
2023-01-12 09:54:04 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,10 @@ if (typeof window !== 'undefined') {
 function Website({ Component, pageProps, router }) {
   return (
     <Chakra cookies={pageProps.cookies}>
+      <Head>
+      <script async defer src="https://analytics.umami.is/script.js" data-website-id="a04c684d-dfbd-42ab-b315-42361601b055"></script>
+     </Head>
+  
       <Fonts />
       <Layout router={router}>
         <AnimatePresence
